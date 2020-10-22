64f71368acd5e6cf688dd923bb3120b3e47d60e5
razelixx <67235257+razelixx@users.noreply.github.com>
2020-10-22 23:04:29 +0300

    Задача 8 - 22/10

---
+++ src/com/company/Main.java
@@ -0,0 +1,27 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        //Способ 1
+        for (int i = 1; i <= 50; i++) {
+            System.out.println(i);
+        }
+
+        //Способ 2
+//        int i = 1;
+//        do {
+//            System.out.println(i);
+//            i++;
+//        } while (i < 51);
+
+        //Способ 3
+//        int i = 1;
+//        while (i < 51) {
+//            System.out.println(i);
+//            i++;
+//        }
+
+    }
+}
